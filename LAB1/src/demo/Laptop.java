afc66f4de9e3a9397deafa166c9da33b60a45c62
Roman Spodaryk <roman.spodaryk@ukr.net>
2019-02-11 22:32:35 +0200

    chande indents in class

--- LAB1/src/demo/Laptop.java
+++ LAB1/src/demo/Laptop.java
@@ -1,120 +1,122 @@
 package demo;
 
 public class Laptop {
-	
-private double speedOfCPU;
-private double capacityOfRAM;
-private String name;
-private double sizeOfDisplay;
-private double price;	
 
-protected int number;
-protected double weight;
+    private double speedOfCPU;
+    private double capacityOfRAM;
+    private String name;
+    private double sizeOfDisplay;
+    private double price;
 
-static int numberOfMonitors = 1;
+    protected int number;
+    protected double weight;
 
-public Laptop() {
+    static int numberOfMonitors = 1;
 
-}
+    public Laptop() {
 
-public Laptop(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay, double price, int number, double weight) {
-	this.speedOfCPU = speedOfCPU;
-	this.capacityOfRAM = capacityOfRAM;
-	this.name = name;
-	this.sizeOfDisplay = sizeOfDisplay;
-	this.price = price;
-	this.number = number;
-	this.weight = weight;
-}
+    }
 
-public Laptop(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay) {
-	this(speedOfCPU, capacityOfRAM, name, sizeOfDisplay, 0.0, 0, 0.0);
-}
+    public Laptop(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay, double price, int number,
+            double weight) {
+        this.speedOfCPU = speedOfCPU;
+        this.capacityOfRAM = capacityOfRAM;
+        this.name = name;
+        this.sizeOfDisplay = sizeOfDisplay;
+        this.price = price;
+        this.number = number;
+        this.weight = weight;
+    }
 
-public String toString() {
-	String information;
-	information = "name = "+name+"\n";
-	information += "speed of CPU = "+speedOfCPU+" GHz"+"\n";
-	information +="capacity of RAM = "+capacityOfRAM+" GB"+"\n";
-	information +="size of display = "+sizeOfDisplay+"″"+"\n";
-	information +="price = "+price+" ₴"+"\n";
-	information +="number = "+number+"\n";
-	information +="weight = "+weight+" kg"+"\n";
-	return information;
-}
+    public Laptop(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay) {
+        this(speedOfCPU, capacityOfRAM, name, sizeOfDisplay, 0.0, 0, 0.0);
+    }
 
-public static void printStatic​NumberOfMonitors() {
-	System.out.println("number of monitors = "+numberOfMonitors);
-}
+    public String toString() {
+        String information;
+        information = "name = " + name + "\n";
+        information += "speed of CPU = " + speedOfCPU + " GHz" + "\n";
+        information += "capacity of RAM = " + capacityOfRAM + " GB" + "\n";
+        information += "size of display = " + sizeOfDisplay + "″" + "\n";
+        information += "price = " + price + " ₴" + "\n";
+        information += "number = " + number + "\n";
+        information += "weight = " + weight + " kg" + "\n";
+        return information;
+    }
 
-public void printNumberOfMonitors() {
-	System.out.println("number of monitors = "+numberOfMonitors);
-}
+    public static void printStatic​NumberOfMonitors() {
+        System.out.println("number of monitors = " + numberOfMonitors);
+    }
 
-public void resetValues(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay, double price, int number, double weight) {
-	this.speedOfCPU = speedOfCPU;
-	this.capacityOfRAM = capacityOfRAM;
-	this.name = name;
-	this.sizeOfDisplay = sizeOfDisplay;
-	this.price = price;
-	this.number = number;
-	this.weight = weight;
-}
+    public void printNumberOfMonitors() {
+        System.out.println("number of monitors = " + numberOfMonitors);
+    }
 
-public double getSpeedOfCPU() {
-	return speedOfCPU;
-}
+    public void resetValues(double speedOfCPU, double capacityOfRAM, String name, double sizeOfDisplay, double price,
+            int number, double weight) {
+        this.speedOfCPU = speedOfCPU;
+        this.capacityOfRAM = capacityOfRAM;
+        this.name = name;
+        this.sizeOfDisplay = sizeOfDisplay;
+        this.price = price;
+        this.number = number;
+        this.weight = weight;
+    }
 
-public void setSpeedOfCPU(double speedOfCPU) {
-	this.speedOfCPU = speedOfCPU;
-}
+    public double getSpeedOfCPU() {
+        return speedOfCPU;
+    }
 
-public double getCapacityOfRAM() {
-	return capacityOfRAM;
-}
+    public void setSpeedOfCPU(double speedOfCPU) {
+        this.speedOfCPU = speedOfCPU;
+    }
 
-public void setCapacityOfRAM(double capacityOfRAM) {
-	this.capacityOfRAM = capacityOfRAM;
-}
+    public double getCapacityOfRAM() {
+        return capacityOfRAM;
+    }
 
-public String getName() {
-	return name;
-}
+    public void setCapacityOfRAM(double capacityOfRAM) {
+        this.capacityOfRAM = capacityOfRAM;
+    }
 
-public void setName(String name) {
-	this.name = name;
-}
+    public String getName() {
+        return name;
+    }
 
-public double getSizeOfDisplay() {
-	return sizeOfDisplay;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public void setSizeOfDisplay(double sizeOfDisplay) {
-	this.sizeOfDisplay = sizeOfDisplay;
-}
+    public double getSizeOfDisplay() {
+        return sizeOfDisplay;
+    }
 
-public double getPrice() {
-	return price;
-}
+    public void setSizeOfDisplay(double sizeOfDisplay) {
+        this.sizeOfDisplay = sizeOfDisplay;
+    }
 
-public void setPrice(double price) {
-	this.price = price;
-}
+    public double getPrice() {
+        return price;
+    }
 
-public int getNumber() {
-	return number;
-}
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-public void setNumber(int number) {
-	this.number = number;
-}
+    public int getNumber() {
+        return number;
+    }
 
-public double getWeight() {
-	return weight;
-}
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
-public void setWeight(double weight) {
-	this.weight = weight;
-}
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
 }
--- LAB1/src/demo/Main.java
+++ LAB1/src/demo/Main.java
@@ -2,18 +2,18 @@ package demo;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Laptop laptop1 = new Laptop();
-		Laptop laptop2 = new Laptop(3.5, 8, "ASUS", 12);
-		Laptop laptop3 = new Laptop(3.6, 16, "MacBook", 15, 47000, 30, 1.83);
-		
-	    System.out.println(laptop1.toString());
-	    System.out.println(laptop2.toString());
-	    System.out.println(laptop3.toString());
+    public static void main(String[] args) {
 
-		Laptop.printStatic​NumberOfMonitors();
-		laptop1.printNumberOfMonitors();
-	}
+        Laptop laptop1 = new Laptop();
+        Laptop laptop2 = new Laptop(3.5, 8, "ASUS", 12);
+        Laptop laptop3 = new Laptop(3.6, 16, "MacBook", 15, 47000, 30, 1.83);
+
+        System.out.println(laptop1.toString());
+        System.out.println(laptop2.toString());
+        System.out.println(laptop3.toString());
+
+        Laptop.printStatic​NumberOfMonitors();
+        laptop1.printNumberOfMonitors();
+    }
 
 }
